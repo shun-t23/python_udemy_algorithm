2e6dbbb801d78c6237b505ea8978088f73a81b4f
shunsuke <shuntanaka01@outlook.jp>
2024-08-07 14:26:00 +0900

    [add]マージソートの追加

---
+++ sort/merge/sort.py
@@ -0,0 +1,44 @@
+# マージソート
+from typing import List
+
+def merge_sort(numbers:List[int]) -> List[int]:
+  if len(numbers) <= 1:
+    return numbers
+  
+  # スラッシュ2つでintegerにしている
+  center = len(numbers) //2
+  # centerで分割し、その左側を取得
+  left = numbers[:center]
+  right = numbers[center:]
+
+  merge_sort(left)
+  merge_sort(right)
+
+  i = j = k =0
+  while i < len(left) and j <len(right):
+    if left[i] < right[j]:
+      numbers[k] = left[i]
+      i += 1
+    else:
+      numbers[k] = right[j]
+      j += 1
+
+    k += 1
+
+  while i < len(left):
+    numbers[k] = left[i]
+    i += 1
+    k += 1
+  
+ 
+  while j < len(right):
+    numbers[k] = right[j]
+    j += 1
+    k += 1
+
+  return numbers
+
+if __name__ == '__main__':
+  import random
+  nums = [random.randint(0,1000) for z in range(10)]
+  print(merge_sort(nums))
